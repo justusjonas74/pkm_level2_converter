13a6112a66ddc5f3266e6cbeaee9924e428c8b8e
Francis Doege <francis.doege@vvo-online.de>
2023-01-26 12:50:24 +0100

    Added Support for RNTM and PVKM

--- lib/pkm_level2_converter.rb
+++ lib/pkm_level2_converter.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require 'nokogiri'
+require 'pathname'
 require_relative "pkm_level2_converter/version"
 
 module PkmLevel2Converter
@@ -14,17 +15,13 @@ module PkmLevel2Converter
     end
   
     def convertFileName()
-      file_name = @filename
-      #puts file_name
-      f = file_name.split('_')
-      #puts f
+      pn = Pathname.new(@filename)
+      dir, base = File.split(pn)
+      f = base.split('_')
       org_id_l3 = f[1].to_i
-      #puts org_id_l3
       org_id_l2 = org_id_l3 + 0x8000 
-      #puts org_id_l2
       f[1] = org_id_l2.to_s
-      #puts f.join("_")
-      return f.join("_")
+      return Pathname.new(dir).join(f.join("_"))
     end
   
     def save_file(file)
@@ -56,7 +53,17 @@ module PkmLevel2Converter
       orig_file_name = @filename
       if File.file?(orig_file_name) 
         @xml_doc = File.open(orig_file_name) { |f| Nokogiri::XML(f) }
-        convert_ids
+        case @xml_doc.root.name
+        when "pv-km"
+          convert_ids_pvkm
+        when "dl-km"
+          convert_ids_dlkm
+        when "rntm"
+          convert_ids_rntm
+        else
+          puts(@xml_doc.root.name + " wird nicht unterstützt.")
+        end
+
         if xml_is_valid_pkm(@xml_doc) 
           save_file(@xml_doc)
         end
@@ -67,7 +74,18 @@ module PkmLevel2Converter
   
     def xml_is_valid_pkm(xml)
       #xsd =  File.read("./ka/pkm/1/XML-Schema_PKM.xsd")
-      file_path = File.join(File.dirname(__FILE__), './pkm_level2_converter/ka/pkm/1/XML-Schema_PKM.xsd') 
+      path=''
+      case @xml_doc.root.name
+      when "pv-km"
+        path = './pkm_level2_converter/ka/pkm/1/XML-Schema_PKM.xsd'
+      when "dl-km"
+        path = './pkm_level2_converter/ka/pkm/1/XML-Schema_PKM.xsd'
+      when "rntm"
+        path = './pkm_level2_converter/ka/pkm/2/XML-Schema_PKM.xsd'
+      end
+
+      file_path = File.join(File.dirname(__FILE__), path) 
+  
       xsd =  File.read(file_path)
       return self.check_xsd(xsd,xml)
     end
@@ -78,7 +96,7 @@ module PkmLevel2Converter
       map.content = (id + 0x8000).to_s
     end
   
-    def convert_ids
+    def convert_ids_dlkm
       #Organisations-ID des DL: 
       #dl-km/organisation/id
       convert_xpath_l3_id(@xml_doc, 'xmlns:dl-km/xmlns:organisation/xmlns:id')
@@ -101,7 +119,57 @@ module PkmLevel2Converter
       #Für Anzeige von KVP als Klartext:
       #dl-km/kontrollmodul-pool/item/moduldaten/nummerninterpretation-pool/item[nr=2]/nummerntext-pool/item/nr
     end
-  
+
+    def convert_ids_pvkm
+      #Organisations-ID des DL: 
+      #dl-km/organisation/id
+      # convert_xpath_l3_id(@xml_doc, 'xmlns:dl-km/xmlns:organisation/xmlns:id')
+      # puts "Converted following IDs:"
+      # puts "/dl-km/organisation/id = " + @xml_doc.xpath('/xmlns:dl-km/xmlns:organisation/xmlns:id').text
+      
+      # @xml_doc.xpath('/xmlns:dl-km/xmlns:kontrollmodul-pool/xmlns:item').each do |node|
+        node = @xml_doc.xpath('/xmlns:pv-km')
+        #Organisations-ID des PV:
+        #dl-km/kontrollmodul-pool/item/moduldaten/organisation/id
+        # convert_xpath_l3_id(node, '/xmlns:pv-km/xmlns:organisation/xmlns:id')
+        convert_xpath_l3_id(node, 'xmlns:organisation/xmlns:id')
+        puts "# pv-km/organisation/id = " + node.xpath('xmlns:organisation/xmlns:id').text
+        #Für PVKM zulässige Organisations-IDs der DL:
+        #dl-km/kontrollmodul-pool/item/moduldaten/organisation-pool/item/id
+        node.xpath('xmlns:organisation-pool/xmlns:item').each do |child| 
+          convert_xpath_l3_id(child, 'xmlns:id')
+          puts "## pv-km/organisation-pool/item/id = " + child.xpath('xmlns:id').text
+        end
+      # end
+      #Für Anzeige von KVP als Klartext:
+      #dl-km/kontrollmodul-pool/item/moduldaten/nummerninterpretation-pool/item[nr=2]/nummerntext-pool/item/nr
+    end
+    
+    def convert_ids_rntm
+      #Organisations-ID des RN: 
+      #rntm/herausgeber/nr
+      convert_xpath_l3_id(@xml_doc, 'xmlns:rntm/xmlns:herausgeber/xmlns:nr')
+      puts "Converted following IDs:"
+      puts "rntm/herausgeber/nr = " + @xml_doc.xpath('xmlns:rntm/xmlns:herausgeber/xmlns:nr').text
+      
+      @xml_doc.xpath('/xmlns:rntm/xmlns:tarifmodul-pool/xmlns:item').each do |node|
+      
+        #Organisations-ID des PV:
+        #dl-km/kontrollmodul-pool/item/moduldaten/organisation/id
+        
+        convert_xpath_l3_id(node, 'xmlns:tarifmodul/xmlns:herausgeber/xmlns:nr')
+        puts "# /rntm/tarifmodul-pool/item/tarifmodul/herausgeber/nr/ = " + node.xpath('xmlns:tarifmodul/xmlns:herausgeber/xmlns:nr').text
+        #Für PVKM zulässige Organisations-IDs der DL:
+        #dl-km/kontrollmodul-pool/item/moduldaten/organisation-pool/item/id
+        node.xpath('xmlns:tarifmodul/xmlns:organisation-pool/xmlns:item').each do |child| 
+          convert_xpath_l3_id(child, 'xmlns:nr')
+          puts "## /rntm/tarifmodul-pool/item/tarifmodul/organisation-pool/item/nr = " + child.xpath('xmlns:nr').text
+        end
+      end
+      #Für Anzeige von KVP als Klartext:
+      #dl-km/kontrollmodul-pool/item/moduldaten/nummerninterpretation-pool/item[nr=2]/nummerntext-pool/item/nr
+    end
+
   end
   
 end
