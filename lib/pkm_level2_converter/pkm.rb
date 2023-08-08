8d63f7b4bff05d80a1210354dee30fac3833f056
Francis Doege <francis.doege@web.de>
2023-08-08 10:57:49 +0200

    Hotfix v0.3.2

--- lib/pkm_level2_converter/pkm.rb
+++ lib/pkm_level2_converter/pkm.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative 'pools'
-require 'pry'
 
 # Main Class
 class PKM
--- lib/pkm_level2_converter/types.rb
+++ lib/pkm_level2_converter/types.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'pry'
-
 # Generische Klasse Poolelement
 class Poolelement
   class << self
